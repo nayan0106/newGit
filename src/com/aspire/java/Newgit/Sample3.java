12b8927b98e7e1e087d72936856ceb05ddd53795
Suraj <Suraj@DESKTOP-SQEPMF0>
2022-07-25 10:26:41 +0530

    hii second class

--- src/com/aspire/java/Newgit/Sample3.java
+++ src/com/aspire/java/Newgit/Sample3.java
@@ -2,8 +2,13 @@ package com.aspire.java.Newgit;
 
 public class Sample3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int a=20;
+		int b=60;
+		int sub=b-a;
+		System.out.println(sub);
+		
 
 	}
 
