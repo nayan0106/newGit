0a72b6c0080b43b5527b705aa90c18cf440b41f0
Suraj <Suraj@DESKTOP-SQEPMF0>
2022-07-25 10:16:25 +0530

    "hii this is my second commit"

---
+++ src/com/aspire/java/Newgit/Sample2.java
@@ -0,0 +1,15 @@
+package com.aspire.java.Newgit;
+
+public class Sample2 {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		 int multi=a*b;
+		 System.out.println(multi);
+		
+
+	}
+
+}
